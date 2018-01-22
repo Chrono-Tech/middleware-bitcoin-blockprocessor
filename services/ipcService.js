cc9f5171ac97f6f95db2f3a3cb6c2177ad34b2e0
zyevegor <zyev.egor@gmail.com>
2018-01-22 15:13:22 +0300

    https://chronobank.atlassian.net/browse/MD-134 - bitcoin balance calculation

--- services/ipcService.js
+++ services/ipcService.js
@@ -51,73 +51,7 @@ const init = async node => {
     );
   });
 
-  node.rpc.add('getblockrangetxbyaddress', async (...args) => {
-    args = args[0];
-    const address = args[0];
-    const delta = args[1];
-
-    const height = await node.rpc.getBlockCount([]);
-
-    let lastBlocks = await Promise.map(_.map(new Array(delta), (item, iter) => iter), async (iter) => {
-      return await node.rpc.getBlockByHeight([height - iter]);
-    });
-
-    let txs = _.chain(lastBlocks)
-      .map(block => block.tx)
-      .flattenDeep()
-      .value();
-
-    let lastTxs = await Promise.map(txs, async (txid) => {
-      let tx = await node.rpc.getRawTransaction([txid, true]);
-
-      tx.vin = _.filter(tx.vin, vin => vin.vout);
-
-      tx.inputs = await Promise.map(tx.vin, async vin => {
-        let tx = await node.rpc.getRawTransaction([vin.txid, true]);
-        return tx.vout[vin.vout];
-      });
-
-      tx.inputs = _.compact(tx.inputs);
-
-      tx.outputs = tx.vout.map(v => ({
-        value: Math.floor(v.value * Math.pow(10, 8)),
-        scriptPubKey: v.scriptPubKey,
-        addresses: v.scriptPubKey.addresses
-      }));
-
-      for (let i = 0; i < tx.inputs.length; i++) {
-        tx.inputs[i] = {
-          addresses: tx.inputs[i].scriptPubKey.addresses,
-          prev_hash: tx.vin[i].txid, //eslint-disable-line
-          script: tx.inputs[i].scriptPubKey,
-          value: Math.floor(tx.inputs[i].value * Math.pow(10, 8)),
-          output_index: tx.vin[i].vout //eslint-disable-line
-        };
-      }
-
-
-
-      let inputIncludes = _.find(tx.inputs, input=> _.get(input, 'addresses', []).includes(address));
-      let outputIncludes = _.find(tx.outputs, input=> _.get(input, 'addresses', []).includes(address));
-
-      return inputIncludes || outputIncludes ? tx : null;
-    });
-
-    return _.compact(lastTxs);
-
-  });
-
   node.rpc.add('getmetabyaddress', node.getMetaByAddress.bind(node));
-
-  node.rpc.add('getcoin', async (...args)=>{
-    args = args[0];
-    return await node.getCoin(...args);
-  });
-
-  node.rpc.add('getmeta', async (...args)=>{
-    args = args[0];
-    return await node.getMeta(...args);
-  });
 
   ipc.serve(() => {
     ipc.server.on('message', async (data, socket) => {
@@ -127,7 +61,6 @@ const init = async node => {
 
         ipc.server.emit(socket, 'message', {result: json, id: data.id});
       } catch (e) {
-        console.log(e);
         ipc.server.emit(socket, 'message', {
           result: null,
           error: {
