597ff69a3e7326999fbffb2b97633bccb649c927
zyevegor <zyev.egor@gmail.com>
2018-03-27 15:12:30 +0300

    https://chronobank.atlassian.net/browse/MD-250 - bitcoin-ipc

---
+++ utils/getUTXO.js
@@ -0,0 +1,29 @@
+const config = require('../config'),
+  Promise = require('bluebird'),
+  ipcExec = require('../services/ipcExec'),
+  blockModel = require('../models/blockModel'),
+  _ = require('lodash');
+
+module.exports = async (block) => {
+
+  const toCreate = _.chain(block.txs)
+    .map(tx =>
+      tx.outputs.map((output, index) =>
+        _.merge(output, {hash: tx.hash, index: index, blockNumber: block.number})
+      )
+    )
+    .flattenDeep()
+    .value();
+
+  let outs = await Promise.map(toCreate, async output => {
+    let isSpent = await blockModel.count({
+      number: output.blockNumber,
+      'txs.inputs.prevout.hash': output.hash,
+      'txs.inputs.prevout.index': output.index
+    });
+    return isSpent ? null : output;
+  });
+
+  return _.compact(outs);
+
+};
