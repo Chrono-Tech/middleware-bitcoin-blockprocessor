0c644300cd443e09582a33524b884b7dd64e15f0
kirya <cloudkserg11@gmail.com>
2018-05-10 10:07:25 +0300

    https://chronobank.atlassian.net/browse/MD-306 - middleware bitcoin - blockprocessor, lightweight change provider ipc

--- utils/ipcExec.js
+++ utils/ipcExec.js
@@ -9,53 +9,55 @@ const Promise = require('bluebird'),
   uniqid = require('uniqid'),
   ipc = require('node-ipc');
 
-
-
-
-
-module.exports = async (uri, method, params) => {
-  const ipcInstance = new ipc.IPC;
+class ipcExec {
+  constructor(uri) {
+    this.ipcInstance = new ipc.IPC;
   
-  const callbacks = {};
-  const ipcPath = path.parse(uri);
+    this.callbacks = {};
+    this.ipcPath = path.parse(uri);
+  
+    Object.assign(this.ipcInstance.config, {
+      id: uniqid(),
+      socketRoot: `${this.ipcPath.dir}/`,
+      retry: 1500,
+      sync: false,
+      silent: true,
+      unlink: true
+    });
+    
+    this.ipcInstance.connectTo(this.ipcPath.base);
+    
+    this.ipcInstance.of[this.ipcPath.base].on('message', async data => {
+      if (!data.error) {
+        this.callbacks[data.id](null, data.result);
+        delete this.callbacks[data.id];
+        return;
+      }
+    
+      this.callbacks[data.id](data.error);
+      delete this.callbacks[data.id];
+    });
+    
+    this.ipcInstance.of[this.ipcPath.base].on('error', async err => {
+      for (let key of Object.keys(this.callbacks)) {
+        this.callbacks[key](err);
+        delete this.callbacks[key];
+      }
+    });
+  }
 
-  Object.assign(ipcInstance.config, {
-    id: uniqid(),
-    socketRoot: `${ipcPath.dir}/`,
-    retry: 1500,
-    sync: false,
-    silent: true,
-    unlink: true
-  });
+  async execMethod(method, params) {
+    return new Promise((res, rej) => {
+      const requestId = uniqid();
+      this.callbacks[requestId] = (err, result) => err ? rej(err) : res(result);
   
-  ipcInstance.connectTo(ipcPath.base);
-  
-  ipcInstance.of[ipcPath.base].on('message', async data => {
-    if (!data.error) {
-      callbacks[data.id](null, data.result);
-      delete callbacks[data.id];
-      return;
-    }
-  
-    callbacks[data.id](data.error);
-    delete callbacks[data.id];
-  });
-  
-  ipcInstance.of[ipcPath.base].on('error', async err => {
-    for (let key of Object.keys(callbacks)) {
-      callbacks[key](err);
-      delete callbacks[key];
-    }
-  });
+      this.ipcInstance.of[this.ipcPath.base].emit('message', JSON.stringify({
+        method: method,
+        params: params,
+        id: requestId
+      }));
+    });
+  }
+}
 
-  return new Promise((res, rej) => {
-    const requestId = uniqid();
-    callbacks[requestId] = (err, result) => err ? rej(err) : res(result);
-
-    ipcInstance.of[ipcPath.base].emit('message', JSON.stringify({
-      method: method,
-      params: params,
-      id: requestId
-    }));
-  });
-};
+module.exports = ipcExec;
