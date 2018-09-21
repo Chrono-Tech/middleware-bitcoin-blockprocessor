ad5d6faa51492e947bddd01dd33f5b3c8d60cb84
kirya <cloudkserg11@gmail.com>
2018-09-21 20:01:50 +0300

    fixed

--- tests/features/index.js
+++ tests/features/index.js
@@ -25,7 +25,7 @@ module.exports = (ctx) => {
     await models.coinModel.remove({});
     await models.accountModel.remove({});
 
-    ctx.blockProcessorPid = spawn('node', ['index.js'], {env: process.env, stdio: 'inherit'});
+    ctx.blockProcessorPid = spawn('node', ['index.js'], {env: process.env, stdio: 'ignore'});
     await Promise.delay(10000);
   });
 
