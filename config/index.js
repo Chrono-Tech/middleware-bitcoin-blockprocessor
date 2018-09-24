88afe709b2d188ed4a6e3b4373fdab3964bf9403
kirya <cloudkserg11@gmail.com>
2018-09-24 17:11:02 +0300

    https://chronobank.atlassian.net/browse/MD-688 - parseInt to CHECK_SYSTEM

--- config/index.js
+++ config/index.js
@@ -80,7 +80,7 @@ module.exports = {
     exchange: process.env.SYSTEM_RABBIT_EXCHANGE || 'internal',
     serviceName: process.env.SYSTEM_RABBIT_SERVICE_NAME || 'system' 
   },
-  checkSystem: process.env.CHECK_SYSTEM || true,
+  checkSystem: process.env.CHECK_SYSTEM ? parseInt(process.env.CHECK_SYSTEM) : true,
   sync: {
     shadow: parseInt(process.env.SYNC_SHADOW) || true
   },
